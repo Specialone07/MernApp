85aebccb445456af69850d8bcb4f0ffaab4aec79
Bibek Puri <121173799+Specialone07@users.noreply.github.com>
2024-01-06 12:43:51 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,14 @@ app.use(cors({
   credentials:true
 }));
 
-
+  app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 
 //routes 
