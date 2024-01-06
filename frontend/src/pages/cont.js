6095ad3ef8274f3eb3a7b289db9a5e54186d73c0
Bibek Puri <121173799+Specialone07@users.noreply.github.com>
2024-01-06 20:35:14 +0545

    Update cont.js

--- frontend/src/pages/cont.js
+++ frontend/src/pages/cont.js
@@ -47,7 +47,7 @@ const ContactUs = () => {
             Your Name
           </label>
           <textarea
-              type="text"
+              
               id="name"
               name="name"
               className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
@@ -76,7 +76,7 @@ const ContactUs = () => {
             Your Message
           </label>
           <input
-          
+          type="text"
             id="message"
             name="message"
             rows="4"
