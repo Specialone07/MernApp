d499aef35fc23bdb8a97065fa2ae43f8d7348b03
Bibek Puri <121173799+Specialone07@users.noreply.github.com>
2024-01-06 20:58:41 +0545

    Update cont.js

--- frontend/src/pages/cont.js
+++ frontend/src/pages/cont.js
@@ -40,6 +40,8 @@ const ContactUs = () => {
 
   return (
     <div className="container mx-auto mt-8 p-8">
+          <h2 className="text-4xl font-bold text-center">Contact Us</h2>
+
       <form onSubmit={handleSubmit} className="max-w-md mx-auto">
         <div className="mb-4">
           <label htmlFor="name" className="block text-gray-600 text-sm font-medium mb-2">
